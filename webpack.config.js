cde20c90be7a622c60f1dfa59a3feb5fff4744f4
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-22 01:45:37 -0700

    code updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry:'./src/js/index.js',
+    entry:['@babel/polyfill', './src/js/index.js'],
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'js/bundle.js'
@@ -19,6 +19,18 @@ module.exports = {
             
         })
         
-    ]
+    ],
+    
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
     
 };
