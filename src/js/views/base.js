67a5a110b66ad402a5cf97925e9de58280089a82
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-22 01:46:03 -0700

    code updates

---
+++ src/js/views/base.js
@@ -0,0 +1,8 @@
+export const elements = {
+    searchInput: document.querySelector('.search'),
+    searchField: document.querySelector('.search__field'),
+    searchList: document.querySelector('.results__list')
+    
+    
+    
+};
