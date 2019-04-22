e08fb71baf2675d5f1dda4fcadccb0c67a15e408
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-22 02:19:18 -0700

    code updates

--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -16,6 +16,22 @@ export const clearResult = () =>{
     
 }
 
+const limitRecipeTitle=(title, limit = 17)=>{
+    const newTitle = [];
+    if(title.length > limit){
+        title.split(' ').reduce((acc, cur)=>{
+            if(acc + cur.length < limit){
+        newTitle.push(cur);    
+            
+        }                    
+            return acc + cur.length;                    
+                                
+                                }, 0);
+    return `${newTitle.join(' ')}...`;
+        
+    }
+    return title;
+}
 // this one is private funtion no need to export it
 const renderRecipes = recipe =>{
     // no need to write html on one line as we are using template string
@@ -26,7 +42,7 @@ const renderRecipes = recipe =>{
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
