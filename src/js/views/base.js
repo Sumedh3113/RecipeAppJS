8b82778e542d131fa33d6fb50e9da3ce48aae674
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-22 22:58:35 -0700

    update

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,8 +1,33 @@
 export const elements = {
     searchInput: document.querySelector('.search'),
     searchField: document.querySelector('.search__field'),
+    searchres: document.querySelector('.results'),
     searchList: document.querySelector('.results__list')
-    
-    
-    
+
+
+
 };
+
+
+export const renderLoader = parent => {
+    const loader = `
+            <div class= 'loader'>
+                <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+                </svg>
+
+                </div>
+
+            `;
+
+    parent.insertAdjacentHTML('afterbegin', loader);
+
+}
+
+export const clearLoader = () =>{
+    //as loader class is added after search button is clicked
+    const loader = document.querySelector('.loader');
+    if(loader) loader.parentNode.removeChild(loader)
+    
+    
+}
