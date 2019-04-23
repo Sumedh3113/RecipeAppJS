a3f79c0edc561af1ad6b3f35772b0b52ce0f3876
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-23 01:56:18 -0700

    index file

--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -6,6 +6,7 @@ export const getInput = () => elements.searchField.value;
 // clear the search field
 export const clearInput = () => {
     elements.searchField.value = '';
+    
 
 }
 
@@ -13,7 +14,7 @@ export const clearInput = () => {
 export const clearResult = () =>{
     
     elements.searchList.innerHTML = '';
-    
+    elements.searchResPage.innerHTML = "";
 }
 
 const limitRecipeTitle=(title, limit = 17)=>{
@@ -53,7 +54,53 @@ const renderRecipes = recipe =>{
     
 }
 
-export const renderResults = recipes =>{
+//creating buttons
+const CreateButton = (page, type)=>`
+        <button class="btn-inline results__btn--${type}" data-goto=${type==='prev' ? page - 1 : page + 1 }>
+                <span>${type==='prev' ? page - 1 : page + 1 }</span>    
+                <svg class="search__icon">
+                        <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+                    </svg>
+                    
+                </button>                
+`;
+
+// function which will handle pagination
+const renderButton = (page, numResults, resPerPage)=> {
+    const pages = Math.ceil(numResults/resPerPage); // to round 4.5 to 5 pages
+    // pages > 1 operations should occur only if pages are more than 1
+    
+    let button;// declare outside as we want to make it available in all 3 conditions
+    if(page === 1 && pages > 1){
+        // button to go to next page
+        button = CreateButton(page, 'next')
+        
+    }else if(page < pages){
+        // both sides
+        button =`
+            ${CreateButton(page,'prev')}
+            ${CreateButton(page,'next')}
+    
+                `
+    } else if(page === pages && pages > 1 ){
+        // button to go to previous page
+        button = CreateButton(page, 'prev');
+    }
+    
+  elements.searchResPage.insertAdjacentHTML('afterbegin', button);  
+};
+
+
+export const renderResults = (recipes, page = 1, resPerPage=10) =>{
+    // render results of current page
+    // start from 0, 10,20
+    const start = (page - 1)* resPerPage;
+    // end at 10,20,30 as end is not included in slice
+    const end = page * resPerPage;
+    
     // pass each current value to above function
-    recipes.forEach(renderRecipes);
+    recipes.slice(start, end).forEach(renderRecipes);
+    
+    // render pagination button 
+    renderButton(page, recipes.length, resPerPage);
 }
